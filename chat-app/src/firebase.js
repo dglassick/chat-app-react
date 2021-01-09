2e7b515fec7bd7d4109b38fd5cb317ad38b59a94
Derek Glassick <derek.glassick@gmail.com>
2021-01-09 11:53:01 -0800

    created page componenet. Chat rooms generate. Now working on Chat side.

---
+++ chat-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyABP1mBqrSxGkBJe8ImAuqX01efA0_ghL0',
+  authDomain: 'chat-app-12329.firebaseapp.com',
+  projectId: 'chat-app-12329',
+  storageBucket: 'chat-app-12329.appspot.com',
+  messagingSenderId: '407784490548',
+  appId: '1:407784490548:web:f767db2bcb18a156506c61',
+  measurementId: 'G-JTR0BG1Z43'
+};
